0facbc257f58ab39199038d2bbd2842663ab3c31
anatolii.yakushko <shaddyx@gmail.com>
2020-10-04 01:09:55 +0300

    [-] small refactoring

--- fix/beat_detector/main.py
+++ fix/beat_detector/main.py
@@ -3,11 +3,23 @@ import numba
 from scipy.io.wavfile import read, write
 import matplotlib.pyplot as plt
 import argparse
-
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
 parser = argparse.ArgumentParser(description='Process wav file')
 parser.add_argument('file', help='File to process')
-
+parser.add_argument('--show', type=str2bool, default=False, nargs='?', const=True, help='show graph')
 args = parser.parse_args()
+if "." in args.file:
+    baseName = ".".join(args.file.split(".")[:-1])
+else:
+    baseName = args.file
 
 fSampl, data = read(args.file)
 
@@ -16,11 +28,12 @@ d = data[:,0]
 
 print("Freq is :{}".format(fSampl))
 
-peaks_threshold = 800
+peaks_threshold = 600
 secPerSample = 1 / fSampl
 minDelay = 0.15
 multiplier_threshold = 0.58
-
+quick_avg_k = 0.001
+long_avg_k = 0.0001
 @numba.jit()
 def avg(data, mk):
     d1 = []
@@ -50,8 +63,8 @@ def detectPeaks(d1, d2):
 @numba.jit()
 def analyze(data):
     data = [numba.float64(k[0]) for k in data]
-    d1 = avg(data, 0.001)
-    d2 = avg(d1, 0.0003)
+    d1 = avg(data, quick_avg_k)
+    d2 = avg(d1, long_avg_k)
     peaks = detectPeaks(d1, d2)
     return d1, d2, peaks
 
@@ -66,8 +79,9 @@ def showPlot():
     plt.plot(peaks)
     plt.show()
 
+
 print("saving peaks")
-f = open("peaks.txt", "w+")
+f = open(baseName + ".txt", "w+")
 time = 0
 prev = 0
 res = []
@@ -76,8 +90,9 @@ for k in peaks:
     if k > 0:
         f.write("{}: {}\n".format(time, k))
         res.append(time)
-open("peaks.json", "w+").write(json.dumps(res))
+open(baseName + ".json", "w+").write(json.dumps(res))
 
-#showPlot()
+if args.show:
+    showPlot()
 
 
