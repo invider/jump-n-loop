9edf8c3d7b98f6f3a06c19657dfc102ea19c44de
anatolii.yakushko <shaddyx@gmail.com>
2020-10-03 23:25:01 +0300

    [-] fixed timings

--- dna/MusicPlayer.js
+++ dna/MusicPlayer.js
@@ -11,24 +11,26 @@ class MusicPlayer {
         augment(this, df)
         augment(this, st)
         this.lvl = st.lvl || "lvl1";
-        this.beatTimer = new lib.BeatTimer(this.lvl, 0);
-        this.offsetBeatTimer = new lib.BeatTimer(this.lvl, env.tune.spawnOffset - env.tune.musicOffset);
+        this.beatTimer = new lib.BeatTimer(this.lvl);
+        this.offsetBeatTimer = new lib.BeatTimer(this.lvl);
     }
 
     onSpawn() {
     }
     
     evo(dt) {
-        if (!this.started){
-            res.sfx[this.lvl].play();
-            this.started = true;
-        } else {
-            if (this.offsetBeatTimer.evo(dt)){
-                trap("spawnObstacle");
-            }
+        if (this.offsetBeatTimer.evo(dt)){
+            trap("spawnObstacle");
+        }
+
+        if (this.offsetBeatTimer.timer >= env.tune.spawnOffset - env.tune.musicOffset){
             if (this.beatTimer.evo(dt)){
                 trap("beat");
             }
+            if (!this.started){
+                res.sfx[this.lvl].play();
+                this.started = true;
+            } 
         }
     }
 
