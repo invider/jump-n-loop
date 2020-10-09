e5c37d00fd14a6324d7b4d70452fcf9cd7355519
Igor Khotin <khotin@gmx.com>
2020-10-09 21:41:37 +0300

    [*] controls refactoring

--- lab/control/pad.js
+++ lab/control/pad.js
@@ -35,16 +35,27 @@ function evo(dt) {
         let x = d.axes[0] || d.axes[2] || d.axes[4]
         let y = d.axes[1] || d.axes[3] || d.axes[5]
 
-        if (d.buttons[b[0]] && d.buttons[b[0]].pressed) y = -1
-        if (d.buttons[b[1]] && d.buttons[b[1]].pressed) x = -1
-        if (d.buttons[b[2]] && d.buttons[b[2]].pressed) y = 1
-        if (d.buttons[b[3]] && d.buttons[b[3]].pressed) x = 1
+        if (d.buttons[b[1]] && d.buttons[b[1]].pressed) y = -1
+        if (d.buttons[b[2]] && d.buttons[b[2]].pressed) x = -1
+        if (d.buttons[b[3]] && d.buttons[b[3]].pressed) y = 1
+        if (d.buttons[b[4]] && d.buttons[b[4]].pressed) x = 1
 
 
         if (x < -sens) {
             activate(id)
-            p.act(1, id)
+            p.act(2, id)
         } else if (x > sens) {
+            activate(id)
+            p.act(4, id)
+        } else if (isActive(id)) {
+            p.stop(2, id)
+            p.stop(4, id)
+        }
+
+        if (y < -sens) {
+            activate(id)
+            p.act(1, id)
+        } else if (y > sens) {
             activate(id)
             p.act(3, id)
         } else if (isActive(id)) {
@@ -52,30 +63,23 @@ function evo(dt) {
             p.stop(3, id)
         }
 
-        if (y < -sens) {
+        if (d.buttons[b[0]] && d.buttons[b[0]].pressed) {
             activate(id)
             p.act(0, id)
-        } else if (y > sens) {
+        } else if (d.buttons[b[5]] && d.buttons[b[5]].pressed) {
             activate(id)
-            p.act(2, id)
+            p.act(0, id)
         } else if (isActive(id)) {
             p.stop(0, id)
-            p.stop(2, id)
         }
 
-        if (d.buttons[b[4]] && d.buttons[b[4]].pressed) {
-            activate(id)
-            p.act(4, id)
-        } else {
-            p.stop(4, id)
-        }
+        /*
         if (d.buttons[b[5]] && d.buttons[b[5]].pressed) {
             activate(id)
             p.act(5, id)
         } else {
             p.stop(5, id)
         }
-        /*
         if (d.buttons[b[6]] && d.buttons[b[6]].pressed) {
             p.act(6, id)
         } else {
